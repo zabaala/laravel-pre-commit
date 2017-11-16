6efeab663c14092576ec6b2d917c2879f6fa6f60
Mauricio Rodrigues <mauricio.vsr@gmail.com>
2017-11-16 10:43:58 -0300

    initial commit

---
+++ src/PreCommitServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Zabaala\PreCommit;
+
+use Illuminate\Support\ServiceProvider;
+use Zabaala\PreCommit\Commands\PreCommit;
+
+class PreCommitServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerCommand();
+    }
+
+    /**
+     * Register the git:pre-commit command.
+     */
+    private function registerCommand()
+    {
+        $this->app->singleton('command.git.pre-commit', function ($app) {
+            return new PreCommit();
+        });
+
+        $this->commands('command.git.pre-commit');
+    }
+
+}
